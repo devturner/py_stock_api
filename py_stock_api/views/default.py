6477a585eaabf1834d4a93744d7e9af03d8619f9
chris turner <mr.chris.turner@gmail.com>
2018-08-22 17:31:47 -0700

    hello world
    server works and is responding 200

---
+++ py_stock_api/views/default.py
@@ -0,0 +1,11 @@
+from pyramid.response import Response
+from pyramid.view import view_config
+
+
+@view_config(route_name='home', renderer='json', request_method='GET')
+def home_view(request):
+    """ This takes a request and returns a message.
+        request is not used at the moment
+    """
+    message = 'Hello World'
+    return Response(body=message, status=200)
