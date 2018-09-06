797140f0370fb0db86dd2d19c165d36c1c809f3b
chris turner <-v>
2018-09-05 18:49:59 -0700

    adding charts

--- py_stock_api/routes.py
+++ py_stock_api/routes.py
@@ -3,7 +3,7 @@ from pyramid_restful.routers import ViewSetRouter
 from .views.portfolio import PortfolioAPIView
 from .views.portfolio import StockAPIView
 from .views.portfolio import CompanyAPIView
-# from .views.company import CompanyAPIViewset
+# from .views.chart import ChartsAPIView
 from .views.auth import AuthAPIView
 
 
@@ -13,12 +13,12 @@ def includeme(config):
     config.add_route('lookup', '/api/v1/lookup/{symbol}')
 
     router = ViewSetRouter(config, trailing_slash=False)
-    # router.register('api/v1/location', WeatherLocationAPIView, 'location')
     router.register('api/v1/auth/{auth}', AuthAPIView, 'auth')
     router.register('api/v1/stocks/{symbol}', StockAPIView, 'add stock to portfolio')
     router.register('api/v1/stocks', StockAPIView, 'stock portfolio')
     router.register('api/v1/portfolio', PortfolioAPIView, 'Portfolio view')
     router.register('api/v1/company', CompanyAPIView, 'Company view')
+    # router.register('api/v1/charts', ChartsAPIView, 'Charts view')
 
 
 # permission='admin'
